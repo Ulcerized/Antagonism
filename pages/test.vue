703a8f6c0d747da82fb4532ccb8ea4260753da2f
Excruciater <ultraziziboi@gmail.com>
2025-02-07 17:36:23 +0100

    a

--- pages/test.vue
+++ pages/test.vue
@@ -5,27 +5,55 @@ useHead({
   title: "TEST",
 })
 
-const  { data }  = await useFetch('https://rest.bandsintown.com/artists/Antagonism/events?app_id=79ded5ff4c09a57614ecc43f90746887&date=past')
+const { data } = await useFetch('https://rest.bandsintown.com/artists/Antagonism/events?app_id=79ded5ff4c09a57614ecc43f90746887&date=past')
 
-/*let info =  data.value
-let i = 0*/
+let info =  data.value
+let res = Array()
+for (let i=0; i < info.length; i++) {
+  let url = info[i]['url']
+  let datetime = info[i]['datetime']
+  let venuename = info[i]['venue']['name']
+  let venuecity = info[i]['venue']['city']
+  let venuecountry = info[i]['venue']['county']
+  let lineup = info[i]['lineup'].filter(checkAntagonism)
+  let printedlineup = '' 
+  if (lineup.length > 0) {
+    lineup.forEach(band => {
+      printedlineup = printedlineup + ', ' + band
+    });
+  }
 
-let jonhatan = h('div', {innerHTML:'ploup'})
-let magalie = document.createElement('h1')
-magalie.innerHTML = 'la vie de ta mere'
+  let j = h('div', 
+  [h('a', {href: url}, [
+    h('p', [datetime]),
+    h('p', [venuename]),
+    h('p', [venuecity, venuecountry]),
+    h('p', [printedlineup])
+    ])
+  ])
+  res.push(j)
+}
 
-let jonhatan2 = typeof(jonhatan)
+function checkAntagonism(name) {
+  return name != 'Antagonism'
+}
 
+let jonhatan = h('div', [res])
 
 </script>
 
 <template>
   <div>
-    <component :is="jonhatan"/>
-    {{ jonhatan2 }}
-    <component :is="magalie"/>
-    {{ magalie }}
+    <component :is="jonhatan" />
+    <div>
+      <a href="[url]">
+        <p>[datetime]</p>
+        <p>[venue[name]]</p>
+        <p>[venue[city]][venue[country]]</p>
+        <p>[lineup]</p>
+      </a>
     </div>
+  </div>
 </template>
 
 <style></style>
--- pages/tour.vue
+++ pages/tour.vue
@@ -1,14 +1,66 @@
 <template>
   <div class="page tour">
     <h1>CATCH US ON TOUR !</h1>
-    
+    <div class="select">
+      <p v-on:click="chargeDates('future')" class="active">Future</p>
+      <p v-on:click="chargeDates('past')">Past</p>
+    </div>
+    <component :is="jonhatan" />
   </div>
 </template>
 
-<script lang="ts" setup>
+
+<script setup>
+useHead({
+  title: "Tour | Antagonism",
+})
+
+console.log('blaboum')
+
+const { data } = await useFetch('https://rest.bandsintown.com/artists/Antagonism/events?app_id=79ded5ff4c09a57614ecc43f90746887&date=past', { method: 'GET' });
+
+let info = data.value
+let past = Array()
+let months = Array('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+for (let i = 0; i < info.length; i++) {
+  let url = info[i]['url']
+  let datetime = new Date(info[i]['datetime'])
+  let date = months[datetime.getMonth()] + '. ' + datetime.getDate() + ' ' + datetime.getFullYear()
+  let venuename = info[i]['venue']['name']
+  let venuecity = info[i]['venue']['city']
+  let venuecountry = info[i]['venue']['country']
+  let lineup = info[i]['lineup'].filter(checkAntagonism)
+  let lineupYesNo = ''
+  if (lineup.length > 0) {
+    let printedlineup = 'With : '
+    lineup.forEach(band => {
+      printedlineup = printedlineup + band + ', '
+    });
+    printedlineup = printedlineup.slice(0, -2)
+    lineupYesNo = h('p', { class: 'lineup' }, [printedlineup])
+  }
+
+  let j = h('div',
+    [h('a', { href: url, class: 'datesList' },
+      [h('div', { class: 'details' }, [
+        h('p', { class: 'date' }, [date]),
+        h('p', { class: 'venue' }, [venuename, h('br'), venuecity, ', ', venuecountry]),
+        lineupYesNo
+      ]),
+      h('div', { class: 'buttons' }, [
+
+      ])
+      ])
+    ])
+  past.unshift(j)
+}
+
+
+
+let jonhatan = h('div', [past])
+
+
 
 </script>
 
-<style>
-
-</style>
+<style></style>
